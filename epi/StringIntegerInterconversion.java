12c6951e150ea9e0bf51ddcd932ae0dfcee43eb4
unknown <contact@joey.dev>
2019-09-24 15:00:16 -0400

    Complete StringIntegerInterconversion

--- epi/StringIntegerInterconversion.java
+++ epi/StringIntegerInterconversion.java
@@ -7,13 +7,54 @@ import epi.test_framework.TestFailure;
 public class StringIntegerInterconversion {
 
     public static String intToString(int x) {
-        // TODO - you fill in here.
-        return "";
+        if (x == 0) return "0";
+        StringBuilder sb = new StringBuilder();
+        boolean isNegative = (x < 0);
+        boolean minValue = false;
+        int current = Math.abs(x);
+
+        // Unable to get absolute value of Integer.MIN_VALUE - ugly corner case.
+        if (current == Integer.MIN_VALUE) {
+            minValue = true;
+            current = Integer.MAX_VALUE;
+        }
+
+        while (current != 0) {
+            sb.append(current % 10);
+            current = current / 10;
+        }
+
+        if (isNegative) {
+            sb.append('-');
+        }
+        sb = sb.reverse();
+
+        if (minValue) {
+            sb.deleteCharAt(sb.length() - 1);
+            sb.append(8);
+        }
+
+        return sb.toString();
     }
 
     public static int stringToInt(String s) {
-        // TODO - you fill in here.
-        return 0;
+        boolean isNegative = false;
+        if (s.charAt(0) == '-') {
+            isNegative = true;
+        }
+        int i = (isNegative) ? 1 : 0;
+        int num = 0;
+        while (i < s.length()) {
+            if (num == 0) {
+                num += s.charAt(i) - '0';
+            } else {
+                num *= 10;
+                num += s.charAt(i) - '0';
+            }
+            i++;
+        }
+
+        return (isNegative) ? num * - 1 : num;
     }
 
     @EpiTest(testDataFile = "string_integer_interconversion.tsv")
