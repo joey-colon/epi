ba48e326f5a3f4fcfb2d492696f505951b4a692e
unknown <contact@joey.dev>
2019-09-26 23:31:24 -0400

    complete SortedArraysMerge

--- epi/SortedArraysMerge.java
+++ epi/SortedArraysMerge.java
@@ -3,15 +3,48 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+class ArrayElement {
+    public List<Integer> list;
+    public int idx;
+
+    public ArrayElement(List<Integer> list, int idx) {
+        this.list = list;
+        this.idx = idx;
+    }
+}
 
 public class SortedArraysMerge {
-    @EpiTest(testDataFile = "sorted_arrays_merge.tsv")
 
+    @EpiTest(testDataFile = "sorted_arrays_merge.tsv")
     public static List<Integer>
     mergeSortedArrays(List<List<Integer>> sortedArrays) {
-        // TODO - you fill in here.
-        return null;
+        List<Integer> result = new ArrayList<>();
+        Queue<ArrayElement> heap = new PriorityQueue<>(
+                (a, b) -> (a.list.get(a.idx) - b.list.get(b.idx))
+        );
+
+        for (List<Integer> list : sortedArrays) {
+            heap.add(new ArrayElement(list, 0));
+        }
+
+        while (!heap.isEmpty()) {
+            ArrayElement current = heap.poll();
+            result.add(current.list.get(current.idx));
+            current.idx += 1;
+            if (current.list.size() <= current.idx) {
+                continue;
+            } else {
+                heap.offer(current);
+            }
+        }
+
+        return result;
     }
 
     public static void main(String[] args) {
