cba2e7d7a73f014a130bf0e3509536dddd2e4c23
unknown <contact@joey.dev>
2019-09-26 23:31:10 -0400

    complete SearchFirstKey

--- epi/SearchFirstKey.java
+++ epi/SearchFirstKey.java
@@ -9,8 +9,27 @@ public class SearchFirstKey {
     @EpiTest(testDataFile = "search_first_key.tsv")
 
     public static int searchFirstOfK(List<Integer> A, int k) {
-        // TODO - you fill in here.
-        return 0;
+        int result = binarySearch(A, 0, A.size() - 1, k);
+        if (result == -1) return -1;
+        while (result > 0 && A.get(result - 1) == k) {
+            result = binarySearch(A, 0, result-1, k);
+        }
+        return result;
+    }
+
+    public static int binarySearch(List<Integer> a, int low, int high, int key) {
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (a.get(mid) == key) {
+                return mid;
+            } else if (a.get(mid) < key) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return (low < a.size() && a.get(low) == key) ? low : -1;
     }
 
     public static void main(String[] args) {
