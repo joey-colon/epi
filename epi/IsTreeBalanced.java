07addce429d0497e3a64dd861abeba72778aa9a7
unknown <contact@joey.dev>
2019-09-26 22:11:11 -0400

    complete IsTreeBalanced

--- epi/IsTreeBalanced.java
+++ epi/IsTreeBalanced.java
@@ -3,13 +3,23 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
-public class IsTreeBalanced {
+public class    IsTreeBalanced {
 
     @EpiTest(testDataFile = "is_tree_balanced.tsv")
 
     public static boolean isBalanced(BinaryTreeNode<Integer> tree) {
-        // TODO - you fill in here.
-        return true;
+        if (tree == null) return true;
+        return helper(tree) > -1;
+    }
+
+    private static int helper(BinaryTreeNode<Integer> node) {
+        if (node == null) return 0;
+        int left = helper(node.left);
+        int right = helper(node.right);
+
+        if (left == -1 || right == -1) return -1;
+        if (Math.abs(left - right) > 1 ) return -1;
+        return Math.max(left, right) + 1;
     }
 
     public static void main(String[] args) {
