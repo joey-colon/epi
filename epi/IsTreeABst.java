48073a5c0aa96d2f55b00fc17ebceb26e75b1b63
unknown <contact@joey.dev>
2019-09-28 08:50:22 -0400

    complete IsTreeABst

--- epi/IsTreeABst.java
+++ epi/IsTreeABst.java
@@ -7,8 +7,23 @@ public class IsTreeABst {
     @EpiTest(testDataFile = "is_tree_a_bst.tsv")
 
     public static boolean isBinaryTreeBST(BinaryTreeNode<Integer> tree) {
-        // TODO - you fill in here.
-        return true;
+        return isBinaryTreeBST(tree, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    /*
+
+           5
+          / \
+         3   7
+     */
+    private static boolean isBinaryTreeBST(BinaryTreeNode<Integer> node, int lower, int upper) {
+        if (node == null) return true;
+        if (node.data > upper) return false;
+        if (node.data < lower) return false;
+
+        boolean left = isBinaryTreeBST(node.left, lower, node.data - 1);
+        boolean right = isBinaryTreeBST(node.right, node.data + 1, upper);
+        return left && right;
     }
 
     public static void main(String[] args) {
